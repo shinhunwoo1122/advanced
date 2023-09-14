580d6ba7a9373c76ae4c19a9a745349cd0d2c5ac
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-09-14 23:08:10 +0900

    -commit

--- src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -16,6 +16,9 @@ public class OrderRepositoryV2 {
     public void save(TraceId traceId, String itemId){
 
 
+
+
+
         TraceStatus status = null;
         try {
             status = trace.beginSync(traceId ,"OrderRepository.save()");
