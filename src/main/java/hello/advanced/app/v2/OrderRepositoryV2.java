e8cb8e958d21735e055459b8ee546b8ffbd5e3ba
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-09-15 23:39:18 +0900

    -commit

--- src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -15,26 +15,16 @@ public class OrderRepositoryV2 {
 
     public void save(TraceId traceId, String itemId){
 
-
-
-
-
         TraceStatus status = null;
         try {
             status = trace.beginSync(traceId ,"OrderRepository.save()");
-
-
 
             //저장 로직
             if(itemId.equals("ex")){
                 throw new IllegalArgumentException("예외 발생!");
             }
 
-
-
             sleep(1000);
-
-
             trace.end(status);
         }catch (Exception e){
             trace.exception(status, e);
