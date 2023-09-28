3cd783cddb20e7d5fdd624f5c1dfd428f11ff4d3
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-09-29 00:06:17 +0900

    -commit

--- src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -27,6 +27,9 @@ public class OrderRepositoryV2 {
             //저장 로직
             if(itemId.equals("ex")){
                 throw new IllegalArgumentException("예외 발생!");
+
+
+
             }
 
             sleep(1000);
