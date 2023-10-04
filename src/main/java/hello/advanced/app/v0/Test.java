9bbc8a591236a1e3301bcabffe6cf7eb00b7f07b
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-10-04 21:14:36 +0900

    -commit

---
+++ src/main/java/hello/advanced/app/v0/Test.java
@@ -0,0 +1,21 @@
+package hello.advanced.app.v0;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int j, i;
+        for(j = 0, i = 0; i <=5; i++){
+            j+= i;
+            System.out.print(i);
+            if(i == 5){
+                System.out.print("=");
+                System.out.print(j);
+            }else{
+                System.out.print("+");
+            }
+        }
+
+
+    }
+}
