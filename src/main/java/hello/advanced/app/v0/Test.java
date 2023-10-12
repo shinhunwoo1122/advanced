c2e9c6c8cce80aac9789b22d618d152e095e0c0a
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-10-12 22:32:48 +0900

    -commit

--- src/main/java/hello/advanced/app/v0/Test.java
+++ src/main/java/hello/advanced/app/v0/Test.java
@@ -4,11 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
 
-
         int j, i;
-
         for(j = 0, i = 0; i <=5; i++){
-
 
             j+= i;
             System.out.print(i);
