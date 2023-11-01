91e56b43794891e9ae6ca0b8896a58bf12b3972b
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-11-01 22:11:04 +0900

    -commit

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -16,11 +16,6 @@ public class HelloTraceV2 {
     private static final String EX_PREFIX = "<X-";
 
 
-
-
-
-
-
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
