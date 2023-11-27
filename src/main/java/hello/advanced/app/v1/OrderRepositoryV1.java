df8c5120908c64661607aab8719987a63a36b1a8
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-11-27 19:09:08 +0900

    -commit

--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -10,9 +10,6 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
-
-
-
     private final HelloTraceV1 trace;
     public void save(String itemId){
 
