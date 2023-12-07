3b9ee71c0815b8388e814f15536e1ec1ee20205a
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-12-07 18:29:32 +0900

    -commit

--- src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -11,13 +11,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderServiceV2 {
 
-
-
-
-
-
-
-
     private final OrderRepositoryV2 orderRepository;
     private final HelloTraceV2 trace;
 
