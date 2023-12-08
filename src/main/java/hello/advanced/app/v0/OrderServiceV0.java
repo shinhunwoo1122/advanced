e11dac3f2b88b2bac82c7752b0aecdce126ef29f
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-12-08 15:45:43 +0900

    -commit

--- src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV0 {
 
 
-
-
-
     private final OrderRepositoryV0 orderRepository;
 
     public void orderItem(String itemId){
