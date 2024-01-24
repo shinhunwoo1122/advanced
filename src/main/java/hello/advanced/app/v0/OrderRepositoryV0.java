15a3f5aaea0fac45ded838f761b0a49ce13a34cd
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2024-01-24 22:23:53 +0900

    -commit

--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class OrderRepositoryV0 {
-
     public void save(String itemId){
         //저장 로직
         if(itemId.equals("ex")){
